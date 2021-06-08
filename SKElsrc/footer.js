9e928a4e422b50c6335c0d3e0c3f14534a0c5c57
sahabatkreatifel <85544764+sahabatkreatifel@users.noreply.github.com>
2021-06-08 16:08:27 +0700

    Update footer.js

--- SKElsrc/footer.js
+++ SKElsrc/footer.js
@@ -2,7 +2,7 @@ function footerScript(pageLevel){
 	var year = new Date().getFullYear(); var t=".";
 	if (pageLevel == 1){
 		document.write("<div class='copyright'>2020-"+year+"</div>");
-		document.write("<div class ='channelID'><a href='https://www.youtube.com/channel/UCC9z07qxRHUtHBVfFlETohA/' target='_blank'><img class='ytLogo' src='SKElsrc/yt_icon_mono_dark.png'>Rum Onspan Music</a></div>");
+		document.write("<div class ='channelID'><a href='https://www.youtube.com/c/sahabatkreatifel/' target='_blank'><img class='ytLogo' src='SKElsrc/yt_icon_mono_dark.png'>Sahabat Kreatif El</a></div>");
 	} 
 	else if (pageLevel>1){
 		document.write("<div class='copyright'>2020-"+year+"</div>");
